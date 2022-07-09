2881a89e3aa4305851d1a1e0f592981ec0bbb86f
root <root@Kavisha.localdomain>
2022-07-09 14:15:13 +0530

    UCSC 1st Scala Practical test

---
+++ tute22.scala
@@ -0,0 +1,13 @@
+
+object test2{
+
+def main(arg:Array[String]) = {
+var (a:Int,b:Int,c:Int,d:Int) = (2,3,4,5);
+var k:Float = 4.3f;
+
+println (c=c++);
+println(c=(++c)*a++);
+}
+}
+
+
---
+++ tute23.scala
@@ -0,0 +1,32 @@
+import scala.io.StdIn._
+
+
+object test3{
+def  OTC(n:Int):Double = {
+return n*85;
+}
+
+def WHC(n:Int):Double  = {
+return n*250;
+} 
+
+def tax(a:Int,b:Int):Double = {
+return (OTC(a)+WHC(b))*.12;
+}
+def homeSalary(a:Int,b:Int):Double = {
+
+return   OTC(a)+WHC(b) - tax(a,b);
+}
+def main(args:Array[String]){
+      var (ot:Int,wh:Int) = (0,0);
+         ot = readInt();
+         wh = readInt();
+      
+   println("The home salary is "+homeSalary(ot,wh));
+
+        
+
+}
+}
+
+
